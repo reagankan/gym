e2d2b6951816fc9477228bd1a354960d6f08a894
Reagan Kan <reagankan@gmail.com>
2026-01-20 20:45:22 -0800

    plot years as vertical lines. add directory with example plots.

--- plot_utils.py
+++ plot_utils.py
@@ -1,5 +1,38 @@
 import matplotlib.pyplot as plt
 
+def plot_years(dates):
+    
+    prev_year = None
+    ymax = plt.ylim()[1]
+
+    for i, d in enumerate(dates, start=1): # boxplot positions are 1-based
+        year = d[:4]
+
+        if prev_year is not None and year != prev_year:
+            x = i - 0.5 # between years looks nicer
+
+            # vertical line
+            plt.axvline(
+                x=x,
+                linestyle="--",
+                linewidth=1,
+                alpha=0.5
+            )
+
+            # year label at top
+            plt.text(
+                x,
+                ymax,
+                year,
+                ha="center",
+                va="bottom",
+                fontsize=9,
+                alpha=0.7
+            )
+
+        prev_year = year
+
+
 def plot_exercise_boxplot(exercise_name, exercise_data):
     """
     exercise_data: dict[date] -> list[weights]
@@ -12,10 +45,15 @@ def plot_exercise_boxplot(exercise_name, exercise_data):
         return
 
     plt.figure(figsize=(8, 4))
+
     plt.boxplot(weights, labels=dates, showfliers=False)
+    plot_years(dates)
+
     plt.title(exercise_name)
     plt.xlabel("Date")
     plt.ylabel("Weight (lbs)")
+
     plt.xticks(rotation=45)
+
     plt.tight_layout()
     plt.show()
