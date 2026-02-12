84b6d09295ca731a7472afd3fd6be080b8a3060d
Reagan Kan <reagankan@gmail.com>
2026-02-12 11:42:42 -0800

    Add PR plots/labels, fix xticks alignment, enable boxplot outliers

--- plot_utils.py
+++ plot_utils.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def plot_years(dates):
+def annotate_years(dates):
     
     prev_year = None
     ymax = plt.ylim()[1]
@@ -32,28 +32,86 @@ def plot_years(dates):
 
         prev_year = year
 
+def get_personal_record(weights):
+    if not weights:
+        return None, None  # row_idx, max_value
+
+    max_value = None
+    max_row_idx = None
+
+    for i, row in enumerate(weights):
+        if not row:
+            continue
+        row_max = max(row)
+        if (max_value is None) or (row_max > max_value):
+            max_value = row_max
+            max_row_idx = i
+
+    return max_row_idx, max_value
+
+def annotate_personal_record(weights):
+    """
+    Annotate the global max weight across all dates:
+    - horizontal line at max weight
+    - asterisk label at top (aligned with boxplot)
+    """
+    if not weights:
+        return None
+
+    max_row_idx, max_value = get_personal_record(weights)
+    if max_value is None:
+        return None
+
+    # Horizontal PR line
+    plt.axhline(
+        y=max_value,
+        linestyle="-",     # solid line
+        linewidth=2,       # bold
+        color="green",
+    )
+
+    ymax = plt.ylim()[0] # place at bottom of the graph to align with xticks.
+    plt.text(
+        max_row_idx + 1,  # boxplot positions are 1-based
+        ymax,
+        "*",
+        ha="center",
+        va="bottom",
+        fontsize=24,          # make it chonky
+        fontweight="bold",
+        color="green",
+        alpha=0.95
+    )
+
 
 def plot_exercise_boxplot(exercise_name, exercise_data):
     """
     exercise_data: dict[date] -> list[weights]
     """
 
+    # sort things.
     dates = sorted(exercise_data.keys())
     weights = [exercise_data[d] for d in dates]
+    print("exercise_data", exercise_data)
 
     if not weights:
-        return
+        return None
 
+    # size things.
     plt.figure(figsize=(8, 4))
 
-    plt.boxplot(weights, labels=dates, showfliers=False)
-    plot_years(dates)
+    # plot things.
+    plt.boxplot(weights, labels=dates, showfliers=True)
+    annotate_years(dates)
+    annotate_personal_record(weights)
 
-    plt.title(exercise_name)
+    # label things.
+    date_index, pr = get_personal_record(weights)
+    plt.title(f"{exercise_name}\n{dates[date_index]}:{pr}")
     plt.xlabel("Date")
     plt.ylabel("Weight (lbs)")
 
-    plt.xticks(rotation=45)
+    plt.xticks(rotation=45, ha='right', rotation_mode='anchor')
 
     plt.tight_layout()
     plt.show()
